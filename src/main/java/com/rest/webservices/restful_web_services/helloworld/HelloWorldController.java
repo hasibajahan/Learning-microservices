ca520f829b3566dd5dd2b58b22c6593e423c48e9
hasibajahan <hasibajahan786@gmail.com>
2026-07-19 09:57:21 +0530

    Create REST API using path parameter

--- src/main/java/com/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,4 +1,27 @@
 package com.rest.webservices.restful_web_services.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+
+    //creating rest API using path parameter
+
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello %s",name));
+    }
 }
